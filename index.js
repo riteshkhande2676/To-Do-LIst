4f6df9883a4b3a1fe48a0438fca8209f5be78150
rites <riteshkhande2676@gmail.com>
2021-06-03 16:04:47 +0530

    set up the structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const input = documents.getElementById('input');
+const form = documents.getElementById('form');
+
+form.addEventListener('submit ', (e) => {
+    e.preventDefault();
+
+    const todo = input.value;
+
+    if (todo) {
+
+    }
+});
